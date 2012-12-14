8f3fee7a8bc0a8b41ec4ee66d1bf0eed11c58860
Victor <Victor.Bryk@gmail.com>
2012-12-14 14:49:25 +0200

    MenuList update

---
+++ app/assets/javascripts/task.js
@@ -0,0 +1,20 @@
+
+var id_menu = new Array('sub_menu_1');
+startList = function allclose() {
+  for (i=0; i < id_menu.length; i++){
+  document.getElementById(id_menu[i]).style.display = "none";
+}
+}
+function openMenu(id){
+  for (i=0; i < id_menu.length; i++){
+    if (id != id_menu[i]){
+    document.getElementById(id_menu[i]).style.display = "none";
+}
+}
+if (document.getElementById(id).style.display == "block"){
+document.getElementById(id).style.display = "none";
+}else{
+document.getElementById(id).style.display = "block";
+}
+}
+window.onload=startList;
